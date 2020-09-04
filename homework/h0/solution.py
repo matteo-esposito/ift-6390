50d8e90c651c279b38167caa1b3d64c10583f6b6
Matteo Esposito <ds.mesposito@gmail.com>
2020-09-03 23:00:02 -0400

    h0 code done

--- homework/h0/solution.py
+++ homework/h0/solution.py
@@ -2,26 +2,25 @@ import numpy as np
 
 
 def make_array_from_list(some_list):
-    pass
+    return np.array(some_list)
 
 
 def make_array_from_number(num):
-    pass
+    return np.array(list(range(num, num+1)))
 
 
 class NumpyBasics:
     def add_arrays(self, a, b):
-        pass
+        return np.add(a, b)
 
     def add_array_number(self, a, num):
-        pass
+        return np.add(a, make_array_from_number(num))
 
     def multiply_elementwise_arrays(self, a, b):
-        pass
+        return np.multiply(a, b)
 
     def dot_product_arrays(self, a, b):
-        pass
+        return np.dot(a, b)
 
     def dot_1d_array_2d_array(self, a, m):
-        # consider the 2d array to be like a matrix
-        pass
+        return m.dot(a)
